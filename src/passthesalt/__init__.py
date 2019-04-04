e43edc2197a864a6fc3f9a49bf20019fbb7cf3c9
Ross MacArthur <ross@macarthur.io>
2019-04-04 13:22:46 +0200

    Fix * import

--- src/passthesalt/__init__.py
+++ src/passthesalt/__init__.py
@@ -2,6 +2,19 @@
 PassTheSalt is a deterministic password generation and password storage system.
 """
 
+__title__ = 'passthesalt'
+__version__ = '3.1.0'
+__url__ = 'https://github.com/rossmacarthur/passthesalt'
+__author__ = 'Ross MacArthur'
+__author_email__ = 'ross@macarthur.io'
+__license__ = 'MIT'
+__description__ = 'Deterministic password generation and password storage.'
+
+from passthesalt.core import (
+    Algorithm, Config, Encrypted, Generatable, Login, Master, PassTheSalt, Secret
+)
+
+
 __all__ = [
     'Algorithm',
     'Config',
@@ -11,18 +24,6 @@ __all__ = [
     'Master',
     'PassTheSalt',
     'Secret',
-    'error',
+    'exceptions',
     'remote'
 ]
-__title__ = 'passthesalt'
-__version__ = '3.1.0'
-__url__ = 'https://github.com/rossmacarthur/passthesalt'
-__author__ = 'Ross MacArthur'
-__author_email__ = 'ross@macarthur.io'
-__license__ = 'MIT'
-__description__ = 'Deterministic password generation and password storage.'
-
-
-from passthesalt.core import (
-    Algorithm, Config, Encrypted, Generatable, Login, Master, PassTheSalt, Secret
-)
